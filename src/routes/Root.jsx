1a8b41909d4b0a168bf3033be1b454c07685d17e
Byron SeMo <byronsemo@gmail.com>
2023-05-30 22:24:00 -0700

    Add react-router-dom

---
+++ src/routes/Root.jsx
@@ -0,0 +1,13 @@
+import { Header } from '../components/Header'
+import { Main } from '../components/Main'
+import { NavBar } from '../components/NavBar'
+
+export function Root () {
+  return (
+    <>
+      <Header />
+      <NavBar />
+      <Main />
+    </>
+  )
+}

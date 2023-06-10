57aa8d83c72b5561ad39fded7299f018043aa3f1
Byron SeMo <byronsemo@gmail.com>
2023-06-09 21:20:33 -0700

    Add icons

--- src/components/Icons.jsx
+++ src/components/Icons.jsx
@@ -1,5 +1,37 @@
 export function ControllerIcon () {
   return (
-    <svg width='44px' height='44px' viewBox='0 0 192 192' xmlns='http://www.w3.org/2000/svg' fill='none'><g id='SVGRepo_bgCarrier' strokeWidth='0' /><g id='SVGRepo_tracerCarrier' strokeLinecap='round' strokeLinejoin='round' /><g id='SVGRepo_iconCarrier'><path stroke='#f59e0b' strokeLinejoin='round' strokeWidth='12' d='m29.56 78.961-7.14 56.232c-1.441 11.347 12.302 18.085 20.39 9.997L70 118h52l27.19 27.19c8.088 8.088 21.831 1.35 20.39-9.997l-7.14-56.232C159.902 58.978 142.901 44 122.758 44H69.242C49.099 44 32.098 58.978 29.56 78.961Z' /><path stroke='#f59e0b' strokeLinecap='round' strokeWidth='12' d='M66 69v12m0 12V81m0 0H54m12 0h12' /><circle cx='126' cy='69' r='6' fill='#f59e0b' /><circle cx='126' cy='93' r='6' fill='#f59e0b' /><circle cx='138' cy='81' r='6' fill='#f59e0b' transform='rotate(90 138 81)' /><circle cx='114' cy='81' r='6' fill='#f59e0b' transform='rotate(90 114 81)' /></g></svg>
+    <svg width='38px' height='38px' viewBox='0 0 192 192' xmlns='http://www.w3.org/2000/svg' fill='none'><g id='SVGRepo_bgCarrier' strokeWidth='0' /><g id='SVGRepo_tracerCarrier' strokeLinecap='round' strokeLinejoin='round' /><g id='SVGRepo_iconCarrier'><path stroke='#fbbf24' strokeLinejoin='round' strokeWidth='12' d='m29.56 78.961-7.14 56.232c-1.441 11.347 12.302 18.085 20.39 9.997L70 118h52l27.19 27.19c8.088 8.088 21.831 1.35 20.39-9.997l-7.14-56.232C159.902 58.978 142.901 44 122.758 44H69.242C49.099 44 32.098 58.978 29.56 78.961Z' /><path stroke='#fbbf24' strokeLinecap='round' strokeWidth='12' d='M66 69v12m0 12V81m0 0H54m12 0h12' /><circle cx='126' cy='69' r='6' fill='#fbbf24' /><circle cx='126' cy='93' r='6' fill='#fbbf24' /><circle cx='138' cy='81' r='6' fill='#fbbf24' transform='rotate(90 138 81)' /><circle cx='114' cy='81' r='6' fill='#fbbf24' transform='rotate(90 114 81)' /></g></svg>
+  )
+}
+
+export function HomeIcon () {
+  return (
+    <svg width='24px' height='24px' className='w-7 h-7 text-amber-400 transition duration-75  group-hover:text-gray-800 ' viewBox='0 0 24 24' fill='none' xmlns='http://www.w3.org/2000/svg'><g id='SVGRepo_bgCarrier' strokeWidth='0' /><g id='SVGRepo_tracerCarrier' strokeLinecap='round' strokeLinejoin='round' /><g id='SVGRepo_iconCarrier'><path d='M9 20H7C5.89543 20 5 19.1046 5 18V10.9199C5 10.336 5.25513 9.78132 5.69842 9.40136L10.6984 5.11564C11.4474 4.47366 12.5526 4.47366 13.3016 5.11564L18.3016 9.40136C18.7449 9.78132 19 10.336 19 10.9199V18C19 19.1046 18.1046 20 17 20H15M9 20V14C9 13.4477 9.44772 13 10 13H14C14.5523 13 15 13.4477 15 14V20M9 20H15' stroke='currentColor' strokeLinecap='round' strokeLinejoin='round' /></g></svg>
+  )
+}
+
+export function UpcomingIcon () {
+  return (
+    <svg width='24px' height='24px' className='w-6 h-6 text-amber-400 transition duration-75  group-hover:text-gray-800 ' viewBox='0 0 24 24' fill='none' xmlns='http://www.w3.org/2000/svg'><g id='SVGRepo_bgCarrier' strokeWidth='0' /><g id='SVGRepo_tracerCarrier' strokeLinecap='round' strokeLinejoin='round' /><g id='SVGRepo_iconCarrier'> <path d='M3 10.312C3 5.93757 3.93757 5 8.312 5H15.688C20.0624 5 21 5.93757 21 10.312V15.688C21 20.0624 20.0624 21 15.688 21H8.312C3.93757 21 3 20.0624 3 15.688V10.312Z' stroke='currentColor' strokeWidth='2' /> <path d='M6 5L6 3' stroke='currentColor' strokeWidth='2' strokeLinecap='round' /> <path d='M18 5L18 3' stroke='currentColor' strokeWidth='2' strokeLinecap='round' /> <path d='M3.5 9H20.5' stroke='currentColor' strokeWidth='2' strokeLinecap='round' /> </g></svg>
+  )
+}
+
+export function SimilarIcon () {
+  return (
+    <svg aria-hidden='true' className='flex-shrink-0 w-6 h-6  transition duration-75' fill='none' viewBox='0 0 20 20' xmlns='http://www.w3.org/2000/svg'><path stroke='currentColor' d='M5 3a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2V5a2 2 0 00-2-2H5zM5 11a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2v-2a2 2 0 00-2-2H5zM11 5a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2V5zM11 13a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2v-2z' /></svg>
+  )
+}
+
+export function MoblieNavButton () {
+  return (
+    <svg
+      className='w-6 h-6'
+      aria-hidden='true'
+      fill='currentColor'
+      viewBox='0 0 20 20'
+      xmlns='http://www.w3.org/2000/svg'
+    >
+      <path clipRule='evenodd' fillRule='evenodd' d='M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z' />
+    </svg>
   )
 }

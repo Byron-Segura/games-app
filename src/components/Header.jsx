e768980ea811322c8c4779be18ac9fb729eee864
Byron SeMo <byronsemo@gmail.com>
2023-06-13 21:34:22 -0700

    Header styles changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,9 +2,11 @@ import { ControllerIcon } from './Icons'
 
 export function Header () {
   return (
-    <header className='flex flex-row h-20 min-w-full sticky top-0 bg-cyan-950 leading-none p-5'>
-      <ControllerIcon />
-      <h1 className='text-4xl h-8 text-amber-500 mx-1'>GamesLike</h1>
+    <header className='flex flex-row min-w-full sticky top-0 bg-rose-950 leading-none text-start z-50'>
+      <div className='flex flex-row max-w-7xl my-5 mx-3'>
+        <ControllerIcon />
+        <h1 className='text-3xl text-amber-400 mx-1'>Games<strong>Search</strong></h1>
+      </div>
     </header>
   )
 }

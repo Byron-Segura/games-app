050a29f93f6c1861e00bfbd65bbe633d156f61d2
Byron SeMo <byronsemo@gmail.com>
2023-06-16 20:16:08 -0700

    Make useGames.js reusable

--- src/hooks/useGames.js
+++ src/hooks/useGames.js
@@ -1,16 +1,15 @@
 import { useCallback, useState } from 'react'
-import { fetchSimilarGames } from '../services/similar_games/similarGames'
 
 export function useGames () {
   const [loading, setLoading] = useState(false)
   const [, setError] = useState(null)
   const [games, setGames] = useState([])
 
-  const getGames = useCallback(async (search) => {
+  const getGames = useCallback(async (search, fetchAPI) => {
     try {
       setLoading(true)
       setError(null)
-      const newGames = await fetchSimilarGames(search)
+      const newGames = await fetchAPI(search)
       setGames(newGames)
     } catch (e) {
       setError(e)

040f39b796fbd2e0ac664f0c523f47fb4a8babb1
Byron SeMo <byronsemo@gmail.com>
2023-06-20 19:48:53 -0700

    Main.jsx style changes

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,7 +2,7 @@ import { Outlet } from 'react-router-dom'
 
 export function Main () {
   return (
-    <main className='max-w-7xl mx-4 sm:ml-64 sm:mr-12 sm:mt-6 z-10'>
+    <main className='max-w-7xl mx-4 sm:ml-64 sm:mr-6 sm:mt-6 z-10'>
       <Outlet />
     </main>
   )

4f5fd2ee5cd58f1ef68ad51c7165fb64591d51c2
Byron SeMo <byronsemo@gmail.com>
2023-06-20 19:48:15 -0700

    Add LatestReleases.jsx file

---
+++ src/pages/LatestReleases/LatestReleases.jsx
@@ -0,0 +1,8 @@
+export function LatestReleases () {
+  return (
+    <section>
+      <h2 className='text-rose-900  text-3xl text font-bold'>Latest Releases</h2>
+      <p className='mt-4 text-base '>Latest</p>
+    </section>
+  )
+}

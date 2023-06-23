8913d420af69f24a045bedad0e93ae8808b7232a
Byron SeMo <byronsemo@gmail.com>
2023-06-22 20:27:51 -0700

    useSearch.js changes

--- src/hooks/useSearch.js
+++ src/hooks/useSearch.js
@@ -8,21 +8,8 @@ export function useSearch () {
   useEffect(() => {
     if (isFirstInput.current) {
       isFirstInput.current = search === ''
-      return
     }
-
-    if (search === '') {
-      setError('No se puede buscar una película vacía')
-      return
-    }
-
-    if (search.length < 3) {
-      setError('La búsqueda debe tener al menos 3 caracteres')
-      return
-    }
-
-    setError(null)
   }, [search])
 
-  return { search, updateSearch, error }
+  return { search, updateSearch, error, setError }
 }

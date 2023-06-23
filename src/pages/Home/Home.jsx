b855760856b4afe4b09b4bfc53dfac39f4c486d2
Byron SeMo <byronsemo@gmail.com>
2023-06-22 20:27:13 -0700

    Home.jsx style change

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,11 @@
+import { SearchForm } from '../../components/SearchForm'
+import { fetchSearchGame } from '../../services/giantAPI/search_game/fetchSearchGames'
+
 export function Home () {
   return (
     <section>
-      <h2>Home</h2>
+      <h2 className='text-rose-900  text-3xl text font-bold'>Search for a specific game</h2>
+      <SearchForm fetchAPI={fetchSearchGame} labelText='Search game info' />
     </section>
   )
 }

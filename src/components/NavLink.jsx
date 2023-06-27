afd0d3288e4d3f7c197116c60e24d648acabf1fb
Byron SeMo <byronsemo@gmail.com>
2023-06-26 21:37:48 -0700

    Add NavLink.jsx

---
+++ src/components/NavLink.jsx
@@ -0,0 +1,13 @@
+import { NavLink as NavLinkReactRouter } from 'react-router-dom'
+
+export function NavLink ({ to, children, ...props }) {
+  return (
+    <NavLinkReactRouter
+      {...props}
+      className={({ isActive }) => isActive ? 'flex items-center p-2 text-amber-400 rounded-lg hover:bg-stone-700 bg-amber-300/20' : 'flex items-center p-2 text-amber-400 rounded-lg hover:bg-stone-700'}
+      to={to}
+    >
+      {children}
+    </NavLinkReactRouter>
+  )
+}

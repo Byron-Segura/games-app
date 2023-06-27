a557a320bd83ac9e0df88e9a58cf739ee1f0613f
Byron SeMo <byronsemo@gmail.com>
2023-06-26 21:03:16 -0700

    Change Loading.jsx styles

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,7 +1,8 @@
 export function Loading () {
   return (
-    <section className='mx-auto'>
-      <p>Loading...</p>
+    <section className='flex justify-center align-middle gap-4'>
+      <p className='text-xl text-rose-900'>Loading</p>
+      <span className='w-8 h-8 border-2 border-rose-900 border-solid border-transparent border-b-amber-500 rounded-full inline-block box-border animate-spin' />
     </section>
   )
 }

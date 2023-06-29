d45a49c617a50993db6351986e33f49877c84aef
Byron SeMo <byronsemo@gmail.com>
2023-06-28 19:09:16 -0700

    Add new NavLink

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,24 +1,12 @@
 import { useState } from 'react'
-import { NavLink as NavLinkReactRouter } from 'react-router-dom'
-import { HomeIcon, SimilarIcon, UpcomingIcon } from './Icons'
+import { HomeIcon, LatestGamesIcon, MoblieNavButton, SimilarIcon, UpcomingIcon } from './Icons'
+import { NavLink } from './NavLink'
 
 export function NavBar () {
   const [sidebarOpen, setSidebarOpen] = useState(false)
 
   const toggleSidebar = () => {
     setSidebarOpen(!sidebarOpen)
-  }
-
-  const NavLink = ({ to, children, ...props }) => {
-    return (
-      <NavLinkReactRouter
-        {...props}
-        className='flex items-center p-2 text-amber-400 rounded-lg hover:bg-stone-700'
-        to={to}
-      >
-        {children}
-      </NavLinkReactRouter>
-    )
   }
 
   return (
@@ -30,15 +18,7 @@ export function NavBar () {
         onClick={toggleSidebar}
       >
         <span className='sr-only'>Open sidebar</span>
-        <svg
-          className='w-6 h-6'
-          aria-hidden='true'
-          fill='currentColor'
-          viewBox='0 0 20 20'
-          xmlns='http://www.w3.org/2000/svg'
-        >
-          <path clipRule='evenodd' fillRule='evenodd' d='M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z' />
-        </svg>
+        <MoblieNavButton />
       </button>
 
       <aside
@@ -62,6 +42,12 @@ export function NavBar () {
               <NavLink to='upcoming-games'>
                 <UpcomingIcon />
                 <span className='flex-1 ml-3 whitespace-nowrap'>Upcoming Games</span>
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to='latest-releases'>
+                <LatestGamesIcon />
+                <span className='flex-1 ml-3 whitespace-nowrap'>Latest Releases</span>
               </NavLink>
             </li>
           </ul>

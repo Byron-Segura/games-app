31c73a23b40f36e9312b321442cf3db105381674
Byron SeMo <byronsemo@gmail.com>
2023-06-29 21:26:05 -0700

    Remove checkDate and filter

--- src/services/giantAPI/upcoming_games/fetchUpcomingGames.js
+++ src/services/giantAPI/upcoming_games/fetchUpcomingGames.js
@@ -5,38 +5,20 @@ async function getRecentReleases () {
   const { nextYearDate, currentDate } = getDates()
   const API_URL = `https://www.giantbomb.com/api/releases/?api_key=${API_KEY}&format=json&filter=release_date:${currentDate}|${nextYearDate}`
 
-  const checkDate = (year, month, day) => {
-    if (year && month && day) {
-      return year + '-' + month + '-' + day
-    }
-    if (year === null) {
-      return 'Unkown'
-    }
-
-    if (month === null || day === null) {
-      return year
-    }
-  }
-
   try {
     const res = await fetch(getProxyUrl(API_URL))
     const json = await res.json()
 
     const gameData = json.results
 
-    const filteredItems = gameData.filter((game, index, array) => {
-      return array.findIndex(obj => obj.name === game.name) === index
-    })
-
-    return filteredItems.map(game => ({
+    return gameData.map(game => ({
       name: game.name,
       id: game.id,
       cover: game.image.medium_url,
-      release: checkDate(game.expected_release_year, game.expected_release_month, game.expected_release_day)
+      release: [game.expected_release_year, game.expected_release_month, game.expected_release_day]
     }))
   } catch (err) {
-    console.log(err)
-    return null
+    return new Error('Falied to find games :(')
   }
 }
 

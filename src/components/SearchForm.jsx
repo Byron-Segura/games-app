2686b6f0f1374e12c26f8854ae3bedb4aa2c7b62
Byron SeMo <byronsemo@gmail.com>
2023-06-29 21:26:44 -0700

    Remove render of Games from SearchForm.jsx

--- src/components/SearchForm.jsx
+++ src/components/SearchForm.jsx
@@ -1,10 +1,7 @@
-import { useGames } from '../hooks/useGames'
 import { useSearch } from '../hooks/useSearch'
-import { Games } from './Games'
 
-export function SearchForm ({ fetchAPI, labelText }) {
+export function SearchForm ({ onSearch, labelText }) {
   const { search, updateSearch, error } = useSearch()
-  const { getGames, games, loading } = useGames()
 
   const handleChange = (event) => {
     const newSearch = event.target.value
@@ -13,7 +10,7 @@ export function SearchForm ({ fetchAPI, labelText }) {
 
   const handleSubmit = async (event) => {
     event.preventDefault()
-    getGames(search, fetchAPI)
+    onSearch(search)
     updateSearch('')
   }
 
@@ -23,15 +20,10 @@ export function SearchForm ({ fetchAPI, labelText }) {
         <label htmlFor='input' className='text-base text-rose-900'>
           {labelText}
         </label>
-        <br />
         <input onChange={handleChange} name='input' id='input' className='border border-gray-300 focus:border-gray-700 outline-none focus:outline-none h-10 px-3 py-1 hover:outline-none min-w-full mb-4' autoComplete='off' type='text' value={search} required />
 
         {error && <p className='text-red-600'>{error}</p>}
       </form>
-      <main>
-        {loading ? 'Cargando...' : ''}
-        {games.length > 0 ? <Games games={games} /> : ''}
-      </main>
     </section>
   )
 }

50f4a904445c99f6ec2dafed18a9de929e1a8214
Byron SeMo <byronsemo@gmail.com>
2023-06-30 22:39:13 -0700

    Add checkedGames to useGames.js

--- src/hooks/useGames.js
+++ src/hooks/useGames.js
@@ -1,16 +1,19 @@
 import { useCallback, useState } from 'react'
+import { CheckGamesInfo } from './useCheckGames'
 
 export function useGames () {
   const [loading, setLoading] = useState(false)
   const [, setError] = useState(null)
   const [games, setGames] = useState([])
 
-  const getGames = useCallback(async (search, fetchAPI) => {
+  const getGames = useCallback(async (fetchAPI, search) => {
     try {
       setLoading(true)
       setError(null)
       const newGames = await fetchAPI(search)
-      setGames(newGames)
+      const checkedGames = CheckGamesInfo(newGames)
+      console.log(newGames)
+      setGames(checkedGames)
     } catch (e) {
       setError(e)
       console.log('error')

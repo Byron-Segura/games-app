26e76e037c8a9ebccf5901e9fcc390378cb60a55
Byron SeMo <byronsemo@gmail.com>
2023-06-30 22:39:47 -0700

    Add render of Games.jsx to Home.jsx

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,26 @@
+import { Games } from '../../components/Games'
+import { Loading } from '../../components/Loading'
 import { SearchForm } from '../../components/SearchForm'
+import { useGames } from '../../hooks/useGames'
 import { fetchSearchGame } from '../../services/giantAPI/search_game/fetchSearchGames'
 import { Title } from '../shared/Title'
 
 export function Home () {
+  const { getGames, games, loading } = useGames()
+
+  const handleSearch = async (search) => {
+    await getGames(fetchSearchGame, search)
+  }
+
   return (
     <section>
       <Title text='Search for a specific game' />
-      <SearchForm fetchAPI={fetchSearchGame} labelText='Search game info' />
+      <SearchForm onSearch={handleSearch} labelText='Search game info' />
+
+      <main className='mt-8'>
+        {loading ? <Loading /> : ''}
+        {games.length > 0 ? <Games games={games} /> : ''}
+      </main>
     </section>
   )
 }

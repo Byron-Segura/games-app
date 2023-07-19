552748c6f1a016995c731c7880e9cee75191d8a3
Byron SeMo <byronsemo@gmail.com>
2023-07-18 17:53:45 -0700

    Add custom styles to NavLink.jsx

--- src/components/NavLink.jsx
+++ src/components/NavLink.jsx
@@ -1,10 +1,12 @@
 import { NavLink as NavLinkReactRouter } from 'react-router-dom'
 
-export function NavLink ({ to, children, ...props }) {
+export function NavLink ({ to, children, style, ...props }) {
+  const navStyle = style || 'flex items-center p-2 text-amber-400 rounded-lg hover:bg-stone-700'
+
   return (
     <NavLinkReactRouter
       {...props}
-      className={({ isActive }) => isActive ? 'flex items-center p-2 text-amber-400 rounded-lg hover:bg-stone-700 bg-amber-400/10' : 'flex items-center p-2 text-amber-400 rounded-lg hover:bg-stone-700'}
+      className={({ isActive }) => isActive ? (navStyle + ' bg-amber-400/10') : navStyle}
       to={to}
     >
       {children}
